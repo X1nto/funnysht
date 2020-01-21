b760f648719a6be6f1a402b0d4012748f8d31b29
X1nto <burtkni@yandex.ru>
2020-01-21 17:00:21 +0400

    fixed bug

--- funnysht.py
+++ funnysht.py
@@ -1,15 +1,17 @@
-#!/usr/bin/python3.8
+#!/usr/bin/python3.6
 
 import sys
+import os
 from colorama import Fore
 
 if len (sys.argv) != 1 :
    print("Usage:", str(sys.argv[0]))
    sys.exit (1)
 
+
 print(f'{Fore.CYAN}Mini-Program by @X1nto{Fore.RESET}')
 
-print ('why are you gay')
+print ('why are you gay?')
 while True:
    d1a = input ('A)Who says i\'m gay? \nB)I\'m not gay \n[A/B]?:')
    if d1a in ['A', 'B']:
@@ -18,24 +20,32 @@ while True:
 if d1a == "A":
    print('You are gay');
    d1a = input ('A)NO U \n[A?]:')
-   if d1a == "A": 
-      print('ok :(')
-      
+   if d1a == "A":
+      print('ok :(\n')
+
 elif d1a == "B":
    print('Are you Lesbian, Homosexual or transgenda?')
    while True:
-      d1a = input ('A)Lesbian \nB)Homosexual \nC)Transgenda \nD)Wai are u asking sir? \n[A/B/C/D]? :')
-      if d1a in ['A', 'B', 'C', 'D']:
-         break
+       d1a = input ('A)Lesbian \nB)Homosexual \nC)Transgenda \nD)Wai are u asking sir? \n[A/B/C/D]? :')
+       if d1a in ['A', 'B', 'C', 'D']:
+          break
+
+   if d1a == "A":
+      print('Ya like girls?\n')
+   elif d1a == "B":
+       print('gtfo this app ffs\n')
+   elif d1a == "C":
+       print('ya like jazz?\n')
+   elif d1a == "D":
+       print('cuz why not lol\n')
+
+print('wanna try again?')
+while True:
+    d1a = input ('A)Yeah why not \nB)i don\'t play with gays \n[A/B]? :')
+    if d1a in ['A', 'B']:
+       break
+
 if d1a == "A":
-    print("Ya like girls?")
+   os.execl(sys.executable, sys.executable, *sys.argv)
 elif d1a == "B":
-    print('gtfo this app ffs')
-elif d1a == "C":
-    print('ya like jazz?')
-elif d1a == "D":  
-    print('becoz i can! [insert dab emoji here]')
-    
-
-
-
+    print('u hurt meh\n')
